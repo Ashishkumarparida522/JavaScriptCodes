1f238b1a912cbc70cf27d983cf875ba26713f7c5
Ashish kumar parida <140887712+Ashishkumarparida522@users.noreply.github.com>
2025-02-05 05:29:02 +0000

    operatioons on data

---
+++ 02_basics/operations.js
@@ -0,0 +1,23 @@
+let value=2
+let negvalue=-value
+//console.log(negvalue);
+
+
+// let str1="hello"
+// let str2="world"
+// console.log(str1+str2);
+
+
+// console.log(+true)
+// console.log(+"");
+
+
+// let num1,num2,num3;
+// num=num1=num2=num3=2+2
+// console.log(num1);
+
+let gameCounter =100
+let b=gameCounter++;
+console.log(b);
+
+
